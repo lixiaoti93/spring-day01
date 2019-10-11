34eb06d2545d2950d175131c87e4e74f8943feba
lixiaoti93 <1419822259@qq.com>
2019-10-11 22:08:56 +0800

    更新

---
+++ src/main/java/list/ListDemo.java
@@ -0,0 +1,15 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+    public static void main(String[] args) {
+        List list呢 = new ArrayList();
+        list呢.add("list");
+        List list2= new ArrayList();
+        list2.addAll(list呢);
+        System.out.println(list2);
+        System.out.println();
+    }
+}
